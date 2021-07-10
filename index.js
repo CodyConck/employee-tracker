c3ef791cdf3cfb6190e63ee76269fe3e8790d24b
CodyConck <codyconck@gmail.com>
2021-07-10 11:07:55 -0500

    wrote index.js for adding departmen

--- index.js
+++ index.js
@@ -1,49 +1,50 @@
-const inquirer = require('inquirer');
+const { prompt } = require('inquirer');
 const fs = require('fs');
 const dbData = require('./db/connection');
+const db = require('./db/index');
 const { table } = require('console');
 
 function userQuestions() {
-    return inquirer.prompt([{
+    prompt([{
         type: 'list',
         name: 'menu',
         message: 'Plese select one of the following options:',
         choices: ['View All Departments', 'View Roles', 'View Employees', 'Add Departments', 'Add Roles', 'Add Employee', 'Update Employee']
     }])
-    .then((answer) => {
-        switch (answer.menu) {
-            case 'View All Departments':
-                departments();
-                break;
-            case 'View Roles':
-                roles();
-                break;
-            case 'View Employees':
-                employees();
-                break;
-            case 'Add Departments':
-                addDepartmentsPrompt();
-                break;
-            case 'Add Roles':
-                addRoles();
-                break;
-            case 'Add Employee':
-                addEmployee();
-                break;
-            case 'Update Employee':
-                updateEmployee();
-                break;
-        }
-    });
+        .then((answer) => {
+            switch (answer.menu) {
+                case 'View All Departments':
+                    departments();
+                    break;
+                case 'View Roles':
+                    roles();
+                    break;
+                case 'View Employees':
+                    employees();
+                    break;
+                case 'Add Departments':
+                    addDepartmentsPrompt();
+                    break;
+                case 'Add Roles':
+                    addRoles();
+                    break;
+                case 'Add Employee':
+                    addEmployee();
+                    break;
+                case 'Update Employee':
+                    updateEmployee();
+                    break;
+            }
+        });
 }
 
 function addDepartmentsPrompt() {
-    return inquirer.prompt([{
-            type: 'list',
-            name: 'addDepartment',
-            message: ' Would you like to add a department?',
-            choices: ['Yes', 'No']
-        }
+    prompt([{
+        type: 'list',
+        name: 'addDepartment',
+        message: ' Would you like to add a department?',
+        choices: ['Yes', 'No']
+    }
     ]).then((answer) => {
         switch (answer.addDepartment) {
             case 'Yes':
@@ -87,14 +88,15 @@ function employees() {
     })
 };
 
-// function addNewDepartment() {
-//     return inquirer.prompt([{
-//         type: 'input',
-//         name: 'newDeptartment',
-//         message: 'Please enter the name of your new department.'
-//     }]).then(answer.newDepartment.dbData.query(`INSERT INTO department`,(err, result) => {
-//         console.table(result);
-//         return userQuestions();
-//     }))
-// }
+function addNewDepartment() {
+    prompt([{
+        name: 'newDeptartment',
+        message: 'Please enter the name of your new department.'
+    }]).then(res => {
+        let name = res
+        db.createDepartment(name)
+            .then(() => console.log(`added ${name.name} to the db`))
+            .then(() => userQuestions())
+    })
+}
 userQuestions();
